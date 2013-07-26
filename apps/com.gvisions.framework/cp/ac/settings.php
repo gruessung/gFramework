96fead940b7bb3b192c5d1be998d8c701a9ea656
solar22 <alexander@gruessung-online.de>
2013-07-26 08:03:36 +0200

    implement seo_url setting

--- apps/com.gvisions.framework/cp/ac/settings.php
+++ apps/com.gvisions.framework/cp/ac/settings.php
@@ -3,17 +3,29 @@
 <br />
 
 <?php
-  if (isset($_POST["save"]))
+	if (isset($_POST["save"]))
 	{
 		$title = mysql_real_escape_string($_POST["newtitle"]);
 		$db->query("UPDATE ".pfw."_config SET `value` = '$title' WHERE `name` = 'sitetitle'");
+        if ($_POST["seo"])
+        {
+            $db->query("UPDATE ".pfw."_config SET `value` = 'true' WHERE `name` = 'seo_url'");
+            $seo_url = true;
+        }
+        else
+        {
+            $db->query("UPDATE ".pfw."_config SET `value` = 'false' WHERE `name` = 'seo_url'");
+            $seo_url = false;
+        }
 		echo '<div class="alert alert-success">Gespeichert.</div>';
 	}
 	else
 	{
-		$db->query("SELECT value FROM ".pfw."_config WHERE `name` = 'sitetitle'");
-		$title = $db->fetch();
+		$title = sitetitle;
+        $seo_url = seo_url;
 	}
+
+
 ?>
 
 <form action="index.php?action=verwalten&appid=1&ac=settings" method="POST">
@@ -22,12 +34,14 @@
 
 	<tr>
 		<td><legend>Seitentitel</legend><input type="text" name="newtitle" value="<?=$title?>" /></td>
+    </tr>
+    <tr>
+        <td><legend>SEO Url  <a class="label label-warning">Beta</a></legend><input type="checkbox" name="seo" <?php if ($seo_url) echo "checked"; ?>  /> <small>Aktivieren</small></td>
 
 	</tr>
+
 	<tr>
-	</tr>
-	<tr>
-		<td><input class="btn btn-primary" name="save" type="submit" value="Speichern" /></td>
+		<td><br /><br /><input class="btn btn-primary" name="save" type="submit" value="Speichern" /></td>
 	</tr>
 
 </table>
