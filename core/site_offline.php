54456a3e36ef046798be9822b47bead52d5b624e
solar22 <alexander@gruessung-online.de>
2013-10-21 17:00:46 +0200

    "Website offline" Seite überarbeitet

--- core/site_offline.php
+++ core/site_offline.php
@@ -1,5 +1,7 @@
-<title><?=sitetitle?> - Offline</title>
-<center>
-<br><br><br><br>
-<img src="core/cp/images/logo.png" border="0" alt="gvisions control panel"><br>
-Die Website ist momentan offline: <?=site_offline_reason?><br><br><small><a href="core/cp/index.php">gCP Login</a></small>
+<?php
+$template = new gTPL();
+$template->menuid = 0;
+$template->sitename = "Offline";
+$template->text = "<div class=\"alert alert-error\"><b>Oops....</b><br>Diese Website ist gerade offline.<br /><br />".site_offline_reason."<br /><br /><small><a href=\"core/cp/index.php\">gCP Login</a></small></div>";
+$template->show();
+?>
